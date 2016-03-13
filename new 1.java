200a5b4b3fb822f89c989b4897b0726482cb5d21
karolina kopycka <karolina.kopycka.94@gmail.com>
2016-03-13 16:07:15 +0100

    nowy inny plik

---
+++ new 1.java
@@ -0,0 +1,36 @@
+Typy:
+Int, long, float, double, char, Boolean, string.
+Deklaracja zmiennych:
+char litera=’a’;
+string nazwisko=”Kopycka”;
+int a=6, b=3;
+Wypisywanie: System.out.print("String"+zmienna), System.out.println() //nowa linia
+n+=5 to samo co n=n+5
+koniunkcja-&&, alternatywa-||, negacja-!
+Komentarze: /* */ lub //
+	if (warunek1) {
+		instrukcja1;
+		}
+	else if (warunek2) {
+		instrukcja2;
+		}
+	else {
+		instrukcja3;
+		}
+	
+	switch(wyrazenie){
+		case wartosc1: instrukcje; break;
+		case wartosc2: instrukcje; break;
+		case wartosc3: instrukcje; break;
+		default: instrukcje; break;
+	}
+for(inicjalizacja; warunek; krok){
+	instrukcje
+}
+while(warunek){
+	instrukcja
+}
+do
+	instrukcja
+while(warunek)
+
